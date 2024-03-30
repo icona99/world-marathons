9cdb97961eb3abd015144e35f4eb4f50739040e6
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-03-30 06:49:17 +0200

    add angular animations for error message

--- src/app/core/error/error.component.ts
+++ src/app/core/error/error.component.ts
@@ -1,27 +1,41 @@
-// error.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ErrorService } from './error.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  styleUrls: ['./error.component.css'],
+  animations: [
+    trigger('heartbeat', [
+      state('initial', style({
+        transform: 'scale(1)'
+      })),
+      state('final', style({
+        transform: 'scale(1.1)'
+      })),
+      transition('initial<=>final', animate('1000ms ease-in-out'))
+    ])
+  ]
 })
 export class ErrorComponent implements OnInit {
   errorMessage = '';
+  displayError = false;
+  animationState = 'initial';
 
   constructor(private errorService: ErrorService) { }
-
   ngOnInit(): void {
     this.errorService.error$.subscribe((err: any) => {
       this.errorMessage = err?.message || '';
+      this.displayError = true;
+      this.triggerAnimation();
       setTimeout(() => {
-        this.closeError();
-      }, 5000); // Затваряне на грешката след 5 секунди
+        this.displayError = false;
+      }, 6000);
     });
-  }
+  };
 
-  closeError(): void {
-    this.errorMessage = '';
-  }
+  triggerAnimation() {
+    this.animationState = this.animationState === 'initial' ? 'final' : 'initial';
+  };
 }
--- src/app/core/error/error.service.ts
+++ src/app/core/error/error.service.ts
@@ -1,4 +1,4 @@
-// error.service.ts
+
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
