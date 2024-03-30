cdaec5efec0cfa0a0d3b4c55e4511291b01d2ce2
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-03-30 07:48:22 +0200

    add route guard for catalog,create,edit and details pages

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from './auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +10,14 @@ export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    console.log('AuthGuard canActivate isLogged:', this.authService.isLogged); // Извеждане на статуса на логването в конзолата
-    console.log('AuthGuard canActivate state.url:', state.url); // Извеждане на URL адреса в конзолата
-
-    if (this.authService.isLogged) {
-      return true; // Потребителят е логнат - разрешаваме достъпа
-    } else {
-      // Потребителят не е логнат - пренасочваме към страницата за вход със запазване на текущия път
-      this.authService.redirectUrl = state.url;
-      console.log('AuthGuard canActivate redirectUrl:', this.authService.redirectUrl); // Извеждане на пренасочения URL адрес в конзолата
-      return this.router.parseUrl('/login');
-    }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+   boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>
+   {
+    return this.authService.isLogged
   }
-}
 
+   
+}
 
 
 
--- src/app/auth/auth-routing-module.ts
+++ src/app/auth/auth-routing-module.ts
@@ -2,10 +2,11 @@ import {  NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { AuthGuard } from "../auth.guard";
 
 const routes: Routes = [{ path: '', redirectTo: 'login', pathMatch: 'full' },
-    {path:'login',component:LoginComponent},
-{path:'register',component:RegisterComponent}
+    {path:'login',component:LoginComponent,canActivate:[AuthGuard]},
+{path:'register',component:RegisterComponent,canActivate:[AuthGuard]}
 ];
 
 @NgModule({
